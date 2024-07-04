d81234d5266ef6bcff2c7b56701e7b29fec4c2cb
Jonathan Zhang <johnny.zhangyt@gmail.com>
2024-07-04 13:19:02 -0400

    V2 to remove prompt as well

--- content.js
+++ content.js
@@ -1,11 +1,18 @@
 document.addEventListener('DOMContentLoaded', fireContentLoadedEvent, false);
 
 function fireContentLoadedEvent() {
-    let loader = document.querySelector('[bundlename="desktop_rpl_nsfw_blocking_modal"]')
+    let loader = document.querySelector('[bundlename="desktop_rpl_nsfw_blocking_modal"]');
 
     if (loader) {
         console.log(loader);
         loader.remove();
     }
     
+    let prompt = document.querySelector('xpromo-nsfw-blocking-container');
+
+    if (prompt) {
+        const promptRoot = prompt.shadowRoot.querySelector('.prompt');
+
+        promptRoot.remove();
+    }
 }
